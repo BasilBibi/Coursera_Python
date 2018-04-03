aec8400651490143ea036f98174bb9632742b5d1
Basil Bibi <Basil@Tink.connect>
2018-04-03 14:54:55 +0100

    Week06

---
+++ coursera_python/week06_assignment01.py
@@ -0,0 +1,18 @@
+def compute_overtime(h, r):
+    if (h > 40):
+        return r * 1.5 * (h - 40)
+    else:
+        return 0
+
+
+def compute_normalpay(h, r):
+    if( h <= 40 ):
+        return h * r
+    else:
+        return 40 * r
+
+
+def computepay(h, r):
+    overtime = compute_overtime(h, r)
+    normal = compute_normalpay(h, r)
+    return normal + overtime
---
+++ test/test_week06.py
@@ -0,0 +1,30 @@
+import unittest
+from coursera_python.week06_assignment01 import compute_normalpay,compute_overtime,computepay
+
+
+class week06_tests(unittest.TestCase):
+
+    def test_zero_overtime(self):
+        self.assertEqual(0, compute_overtime(0, 10.5))
+
+    def test_one_hour_overtime(self):
+        self.assertEqual(15.75, compute_overtime(41, 10.5))
+
+    def test_5_hours_overtime(self):
+        self.assertEqual(78.75, compute_overtime(45, 10.5))
+
+    def test_zero_normalpay(self):
+        self.assertEqual(0, compute_normalpay(0, 10.5))
+
+    def test_one_hour_normalpay(self):
+        self.assertEqual(10.5, compute_normalpay(1, 10.5))
+
+    def test_normalpay_with_5_hours_extra(self):
+         self.assertEqual(420, compute_normalpay(45, 10.5))
+
+    def test_45_hours_totalpay(self):
+        self.assertEqual(498.75, computepay(45, 10.5))
+
+
+if __name__ == '__main__':
+    unittest.main()
