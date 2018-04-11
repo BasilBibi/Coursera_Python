3626c272e793360c6f16e84e88b5035acb11d62e
Basil Bibi <Basil@Tink.connect>
2018-04-11 11:51:46 +0100

    Added Mod2 week3 assignment 2

--- coursera_python/module2/week3_assignment.py
+++ coursera_python/module2/week3_assignment.py
@@ -9,3 +9,15 @@ def get_sorted_unique_word_list(file):
     unique_words.sort()
     fh.close()
     return unique_words
+
+
+def count_from_lines(file):
+    count = 0
+    fh = open(file)
+    for line in fh:
+        s = line.strip()
+        if "From " in s:
+            count = count + 1
+            l = s.split()
+            print(l[1])
+    return count
--- test/part2/test_week3.py
+++ test/part2/test_week3.py
@@ -1,5 +1,5 @@
 import unittest
-from coursera_python.module2.week3_assignment import get_sorted_unique_word_list
+from coursera_python.module2.week3_assignment import get_sorted_unique_word_list, count_from_lines
 
 
 def get_file_path(fn):
@@ -13,6 +13,10 @@ class Week2Tests (unittest.TestCase):
         expected = ['Arise', 'But', 'It', 'Juliet', 'Who', 'already', 'and', 'breaks', 'east', 'envious', 'fair', 'grief', 'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft', 'sun', 'the', 'through', 'what', 'window', 'with', 'yonder']
         romeo_file = get_file_path('romeo.txt')
         self.assertEqual(expected, get_sorted_unique_word_list(romeo_file))
+
+    def test_get_from_line_count(self):
+        mbox_short_file = get_file_path('mbox-short.txt')
+        self.assertEqual(27, count_from_lines(mbox_short_file))
 
 
 if __name__ == '__main__':
