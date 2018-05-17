a812c0849a993ba3ff3411ea5b7e75a408081e52
Basil <basilbibi@hotmail.com>
2018-05-17 09:16:07 +0100

    More example test manipulations of json

--- test/module3/test_week6_json_parsing.py
+++ test/module3/test_week6_json_parsing.py
@@ -4,44 +4,63 @@ import json
 from test.TestBase import *
 
 
+def get_json_dict_from_file(file_path):
+    geocoding_data = get_file_contents(file_path).encode()
+    j = json.loads(geocoding_data)
+    return j
+
+
 class JsonParsingExamples(unittest.TestCase):
 
     def test_length_results_list(self):
-        geocoding_data = get_file_contents('module3/geocoding_data.json').encode()
-        j = json.loads(geocoding_data)
-        self.assertEqual(20, len( j['results'] ))
+        json_dict = get_json_dict_from_file('module3/geocoding_data.json')
+        addresses = json_dict['results']
+        self.assertEqual(20, len(addresses))
 
     def test_number_of_attribs_in_address(self):
-        geocoding_data = get_file_contents('module3/geocoding_data.json').encode()
-        j = json.loads(geocoding_data)
-        addresses = j['results']
-        addr = addresses[0]
-        self.assertEqual(6, len(addr.keys()) )
+        json_dict = get_json_dict_from_file('module3/geocoding_data.json')
+        addresses = json_dict['results']
+        address = addresses[0]
+        self.assertEqual(6, len(address.keys()))
 
     def test_number_of_attribs_in_address_components(self):
-        geocoding_data = get_file_contents('module3/geocoding_data.json').encode()
-        j = json.loads(geocoding_data)
-        addresses = j['results']
-        addr = addresses[0]
-        addr_comp = addr['address_components']
-        self.assertEqual(7, len(addr_comp) )
+        json_dict = get_json_dict_from_file('module3/geocoding_data.json')
+        addresses = json_dict['results']
+        address = addresses[0]
+        address_component = address['address_components']
+        self.assertEqual(7, len(address_component))
 
-    def test_number_of_attribs_in_address_components(self):
-        geocoding_data = get_file_contents('module3/geocoding_data.json').encode()
-        j = json.loads(geocoding_data)
-        addresses = j['results']
-        addr = addresses[0]
-        addr_comps = addr['address_components']
-        for address_component in addr_comps:
-            long_name = address_component['long_name']
-            print(long_name)
+    def test_number_of_long_names_in_address_component(self):
+        json_dict = get_json_dict_from_file('module3/geocoding_data.json')
+        addresses = json_dict['results']
+        address = addresses[0]
+        address_components = address['address_components']
+        expected_long_names = ['Padua', 'Westpark', 'Irvine', 'Orange County', 'California', 'United States', '92614']
+        actual_long_names = [ac['long_name'] for ac in address_components]
+        self.assertEqual(expected_long_names,  actual_long_names)
 
     def test_formatted_address(self):
-        geocoding_data = get_file_contents('module3/geocoding_data.json').encode()
-        j = json.loads(geocoding_data)
-        addresses = j['results']
-        for address in addresses:
-            print(address['formatted_address'])
+        json_dict = get_json_dict_from_file('module3/geocoding_data.json')
+        addresses = json_dict['results']
+        address = addresses[0]
+        self.assertEqual( 'Padua, Irvine, CA 92614, USA', address['formatted_address'])
+
+    def test_deep_attrib_fetch(self):
+        json_dict = get_json_dict_from_file('module3/geocoding_data.json')
+        nw_bounds_lat = json_dict['results'][0]['geometry']['bounds']['northeast']['lat']
+        self.assertEqual( 33.678201, nw_bounds_lat)
+
+    def test_iterate_deep_attrib_fetch(self):
+        json_dict = get_json_dict_from_file('module3/geocoding_data.json')
+
+        def fetch_location_lat_lng(index):
+            return (json_dict['results'][index]['geometry']['location']['lat'],
+                    json_dict['results'][index]['geometry']['location']['lng'])
+
+        location_lat_lngs = [fetch_location_lat_lng(i)
+                             for i in range(0, len(json_dict['results']))]
+
+        self.assertEqual( 20, len(location_lat_lngs) )
 
 
 if __name__ == '__main__':
